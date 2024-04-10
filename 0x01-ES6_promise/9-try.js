e0e620a190501a716d776f2906409ed432cf373e
yandah1 <nonku003@gmail.com>
2024-04-10 22:48:05 +0000

    modified 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,9 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
-  } finally {
-    queue.push('Guardrail was processed');
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (err) {
+    queue.push(`Error: ${err.message}`, 'Guardrail was processed');
   }
-
   return queue;
 }
