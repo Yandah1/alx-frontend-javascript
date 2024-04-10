dec926a62b2fabecf820894b8ac5fc8af1ba7e8b
yandah1 <nonku003@gmail.com>
2024-04-10 21:37:00 +0000

    Don't make a promise...if you know you can't keep it

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
       const response = { data: 'Response from API' };
       resolve(response);
