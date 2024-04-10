c6347baa64cc03b92b9a37a2509234940b9ec41e
yandah1 <nonku003@gmail.com>
2024-04-10 22:41:42 +0000

    await/async

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,22 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled(promises)
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => {
+      const responseArray = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          responseArray.push({ status: result.status, value: result.value });
+        } else {
+          responseArray.push({
+            status: result.status,
+            value: `Error: ${result.reason.message}`,
+          });
+        }
+      });
+      return responseArray;
+    });
 }
