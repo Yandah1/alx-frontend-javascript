d009326e22a659d3aba2f0b7e66e93160a9a222e
yandah1 <nonku003@gmail.com>
2024-04-11 20:32:25 +0000

    currency

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter and setter for amount attribute
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // Getter and setter for currency attribute
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
