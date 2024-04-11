0f91ef4b9a9497e5da40cf432c57793ccb8ca2cb
yandah1 <nonku003@gmail.com>
2024-04-11 19:57:14 +0000

    . Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  Constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // Getter and setter for code attribute
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  // Getter and setter for name attribute
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
